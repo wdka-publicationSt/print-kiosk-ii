f7be193a3c6001978feda2b023e40d703dc445aa
Castro0o <andre@andrecastro.info>
2018-09-21 15:37:27 +0200

    placed page_pages_dict populating into def

--- download-all-pages.py
+++ download-all-pages.py
@@ -22,31 +22,35 @@ print(site)
 
 f = open('all_pages.json', 'w')
 
-mainpages = site.allpages(namespace=0) # namespace=0 is the Main
-for n, page in enumerate ( list(mainpages) ):
-    page_dict = {}
+def all_pages_dict():
+    mainpages = site.allpages(namespace=0) # namespace=0 is the Main
+    for n, page in enumerate ( list(mainpages) ):
+        pages_dict = {}
+        page_id =  str(page.pageid)
+        pages_dict[page_id] = {} # sub dictionary for each page
+        pages_dict[page_id]["id"] =  page.pageid 
+        pages_dict[page_id]["title"] = page.page_title        
+        pages_dict[page_id]["text"] = page.text() 
+        #print(pages_dict[page_id]["text"])
+        pages_dict[page_id]["extlinks"] = list(page.extlinks())
+        pages_dict[page_id]["categories"] = [cat.page_title for cat in list(page.categories())] # .page_title because categories are a also pages, and presented as such
+        pages_dict[page_id]["lastedittime"] = page.edit_time
+        pages_dict[page_id]["images"] = [img.page_title for img in list(page.images())] #img.name #includes 'File:' in response
+        pages_dict[page_id]["revisions"] = { "recent_revision_user": list(page.revisions())[0]['user'],
+                           "recent_revision_time": list(page.revisions())[0]['timestamp'],
+                           "recent_revision_time_iso":datetime(*(list(page.revisions())[0]['timestamp'])[:6]).isoformat(),
+                           "last_revision_user": list(page.revisions())[-1]['user'],
+                           "last_revision_time": list(page.revisions())[-1]['timestamp'],
+                           "last_revision_time_iso":datetime(*(list(page.revisions())[-1]['timestamp'])[:6]).isoformat()
 
-    page_id =  str(page.pageid)
-    page_dict[page_id] = {}
-    page_dict[page_id]["id"] =  page.pageid 
-    page_dict[page_id]["title"] = page.page_title        
-    page_dict[page_id]["text"] = page.text() 
-    #print(page_dict[page_id]["text"])
-    page_dict[page_id]["extlinks"] = list(page.extlinks())
-    page_dict[page_id]["categories"] = [cat.page_title for cat in list(page.categories())] # .page_title because categories are a also pages, and presented as such
-    page_dict[page_id]["lastedittime"] = page.edit_time
-    page_dict[page_id]["images"] = [img.page_title for img in list(page.images())] #img.name #includes 'File:' in response
-    page_dict[page_id]["revisions"] = { "recent_revision_user": list(page.revisions())[0]['user'],
-                       "recent_revision_time": list(page.revisions())[0]['timestamp'],
-                       "recent_revision_time_iso":datetime(*(list(page.revisions())[0]['timestamp'])[:6]).isoformat(),
-                       "last_revision_user": list(page.revisions())[-1]['user'],
-                       "last_revision_time": list(page.revisions())[-1]['timestamp'],
-                       "last_revision_time_iso":datetime(*(list(page.revisions())[-1]['timestamp'])[:6]).isoformat()
+                           # time info comes in time.struct_time format
+                           # making possible to query with  time.tm_year time.tm_mon
 
-                       # time info comes in time.struct_time format
-                       # making possible to query with  time.tm_year time.tm_mon
+                           }
+        return pages_dict
 
-                       }                
-    json.dump( page_dict, f, indent=4)
-    #pprint(page_dict, indent=4)        
+
+
+json.dump( all_pages_dict(), f, indent=4)    
+    #pprint(pages_dict, indent=4)        
 f.close()
