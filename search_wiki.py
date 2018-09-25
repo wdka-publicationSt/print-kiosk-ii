4535cfd8a0304d1efe821200167db4070987305c
mb@mb <mail@manettaberends.nl>
2018-09-25 12:46:11 +0200

    renaming the search-wiki script to search_wiki, to let python import it

---
+++ search_wiki.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+##### 
+# Script loads the wiki site
+# To be loaded by other scripts that use the wiki APIo
+from wikisite import *
+
+def search_request(query, namespace, reach):
+	search_results = site.search(search=query, namespace='0', what=reach)
+	search_results_titles = []
+	for match in list(search_results): 
+
+	    print('title:', match['title'])
+	    print('snippet:', match['snippet'])
+	    print('\n')
+	    results.append(match['title'])
+	
+	return search_results_titles
