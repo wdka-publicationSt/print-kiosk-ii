24997099874bc57286071c09977b42a9f756e326
mb@mb <mail@manettaberends.nl>
2018-10-05 10:56:00 +0200

    adding questions.py to the git

---
+++ questions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+questions = {
+	'time' : {
+		'question' : 'Are you most productive during normalized working day hours (9am-5pm)?',
+		'variables' : []
+	}
+	'common-words' : {
+		'question' : 'Which words would you expect a Social Practice curriculum or project to have in mind?',
+		'variables' : []
+	}
+	'free-association' : {
+		'question' : 'Please write the first word that comes to your mind.',
+		'variables' : ['activism', 'democracy', 'representation']
+	}
+}
