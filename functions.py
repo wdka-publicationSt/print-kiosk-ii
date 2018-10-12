8ff3e4b59dd99622ca5d35843ffae784ef1b6179
mb@mb <mail@manettaberends.nl>
2018-10-12 16:46:23 +0200

    a place to gather functions

---
+++ functions.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import re
+from bs4 import BeautifulSoup
+from nltk.tokenize import RegexpTokenizer
+tokenizer = RegexpTokenizer(r'\w+') # initialize tokenizer
+
+def remove_wiki_elements(html):
+	soup = BeautifulSoup(html, "lxml")
+
+	# remove the contribute banner + recent articles suggestions banner
+	contribute = soup.find(id='contribute-wrapper')
+	if contribute != None:
+		contribute.clear()
+	suggestions = soup.find_all('div', class_='oneline-wrapper')
+	if len(suggestions) > 0:
+		suggestions[0].clear()
+
+	html = str(soup.body)
+	return html
+
+def parse_html(html):
+	soup = BeautifulSoup(html, "lxml")
+
+	# remove the contribute banner + recent articles suggestions banner
+	contribute = soup.find(id='contribute-wrapper')
+	if contribute != None:
+		contribute.clear()
+	suggestions = soup.find_all('div', class_='oneline-wrapper')
+	if len(suggestions) > 0:
+		suggestions[0].clear()
+
+	plain = soup.text
+	return plain
+
+def tokenize(plain):
+	words = tokenizer.tokenize(plain)
+	return words
+
+def count_article_length(html):
+	plain = parse_html(html)
+	words = tokenize(plain)
+	length = len(words)
+	print('length of this article: ', length)
+	return length
