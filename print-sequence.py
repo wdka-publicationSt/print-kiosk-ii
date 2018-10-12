a8651a6177f9178ec3f10d570149b6aa84b194a1
mb@mb <mail@manettaberends.nl>
2018-10-12 16:50:58 +0200

    small changes in the renaming of files

--- print-sequence.py
+++ print-sequence.py
@@ -16,7 +16,7 @@ articles_index = questionnaire()
 f = open('all_pages.json', 'r').read()
 all_pages = json.loads(f)
 print_queue = {  page: all_pages[page]  for page in articles_index } 
-w = open('queue.json', 'w+')
+w = open('queue.tmp.json', 'w+')
 w.write(json.dumps( print_queue, indent=4))
 w.close()
 print('print_queue contains:', len(print_queue), 'articles')
@@ -24,7 +24,7 @@ print('print_queue contains:', len(print_queue), 'articles')
 # 3 - produce the PDF
 timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 pdf_filename = 'output/latex.queue.{}.pdf'.format(timestamp)
-queue2pdf('all_pages.json', 'queue.json', 'latex.queue.tmp.html', 'latex.metadata.yaml', pdf_filename)
+queue2pdf('all_pages.json', 'queue.tmp.json', 'queue.tmp.html', 'latex.metadata.yaml', pdf_filename)
 
 # 4 - print
 
