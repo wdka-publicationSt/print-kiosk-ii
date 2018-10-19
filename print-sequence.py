52914f3f3f801868446ca2b1e021b60eba273cba
mb@mb <mail@manettaberends.nl>
2018-10-19 09:19:13 +0200

    added the queue2pdf part to the print-sequence again

--- print-sequence.py
+++ print-sequence.py
@@ -14,11 +14,15 @@ articles_index = questionnaire()
 f = open('all_pages.json', 'r').read()
 all_pages = json.loads(f)
 print_queue = {  page: all_pages[page]  for page in articles_index } # create dict from the *articles_index*
-w = open('latex/queue.json','w')
+w = open('queue.tmp.json','w')
 json.dump( print_queue, w, indent=4)
 print('print_queue contains:', len(print_queue), 'articles')
 
 # 3 - produce the PDF
+
+timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+pdf_filename = 'output/latex.queue.{}.pdf'.format(timestamp)
+queue2pdf('all_pages.json', 'queue.tmp.json', 'queue.tmp.html', 'latex.metadata.yaml', pdf_filename)
 
 # 4 - print
 
