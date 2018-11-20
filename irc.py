dda8b78b5d4d6394f57287f4c10eab7b64d4615d
mb@mb <mail@manettaberends.nl>
2018-11-20 14:45:57 +0100

    small changes in the irc script around the selection of a shadow lib pdf

--- irc.py
+++ irc.py
@@ -13,14 +13,13 @@ class bs_irc_user(bot.SimpleBot):
 
 	def on_channel_message(self, event):
 		user = ((event.prefix).split("!"))[0]
-		print(user+":", event.message,  '\n\n\n\n\n\n\n',  file=stdout)
+		print(user+":", event.message, '\n\n\n\n\n\n\n', file=stdout)
 		print("Answer:",  '\n\n\n\n\n\n\n', file=stdout)
 		if "#shadowlibrary" in event.message.lower():
 			selection = re.findall(r'\d\d\d', event.message)
-			print('selection_id', selection,  '\n\n\n\n\n\n\n',  file=stdout)
+			print('I selected this PDF(s) for you #', selection,  '\n\n\n\n\n\n\n',  file=stdout)
 			grep_shadow_pdfs(selection) # execute script that collects the PDFs, and unites them into one a tmp file: shadow_library.pdf
 			# The #shadowlibrary code can only be used once, as it will overwrite the tmp pdf file, each time that it is triggered.
-
 		if "bye" in event.message.lower() or "goodbye" in event.message.lower():
 			print("LEAVE channel",  '\n\n\n\n\n\n\n',  file=stdout)
 			self.part_channel('#beyondsocial', "Bye. See you soon")
@@ -28,7 +27,7 @@ class bs_irc_user(bot.SimpleBot):
 			return # exit the function
 		say = getpass(prompt="") # hack: to get only what user types at this moment
 		# will not echo what s/he is printing atm
-		print (say,  file=stdout)
+		print(say, file=stdout)
 		self.send_message(event.target, say)
 		
 	def on_join(self, event):
@@ -36,16 +35,16 @@ class bs_irc_user(bot.SimpleBot):
 			message = msgformat.bold("/me is here")
 			message = msgformat.color(message, msgformat.RED)
 			self.send_message(event.target, message)
-			print("You are now chatting to ELAINE. Wait for her to start chating with you",  '\n\n\n\n\n\n\n', file=stdout)
+			print("You are now chatting to ELAINE. Wait for her to start chating with you", '\n\n\n\n\n\n\n', file=stdout)
 
 	# Create an instance of the bot
 	# We set the bot's nickname here
 def irc(username):
 	bs_user = bs_irc_user(username)
 		# Let's connect to the host n channel
-	print("Wait a few moments while the connection is being established",  '\n\n\n\n\n\n\n',  file=stdout)
+	print("Wait a few moments while the connection is being established", '\n\n\n\n\n\n\n', file=stdout)
 	bs_user.connect("irc.freenode.com", channel=["#beyondsocial"])
 		# Start running the bot
 	bs_user.start()
 
-irc("bs_user")
+# irc("bs_user")
