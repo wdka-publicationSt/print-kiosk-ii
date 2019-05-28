7c35678d3755e41b51b932fc96d158e63451808e
Castro0o <andre@andrecastro.info>
2019-05-28 18:27:28 +0200

    optized images downloads: downloading only images < 800px high and those that are not yet in images/ dir

--- download-images.py
+++ download-images.py
@@ -8,39 +8,57 @@
 # 1.2.2 if different (the image has changed in server) then  it downloads it
 ###
 
-import os, urllib.request
+import os, urllib.request, json
 from hashlib import sha1
 from wikisite import *
 from pprint import pprint
 
 wiki_images = site.images
-
+api = '{protocol}://{host}{path}api.php?'.format(protocol='https',
+                                                 host=site.host,
+                                                 path=site.path)
+set_hight = 800
 current_dir = os.path.dirname(os.path.realpath(__file__))
-img_dir = os.path.dirname(os.path.realpath(__file__))+'/images/'
+img_dir = os.path.dirname(os.path.realpath(__file__)) + '/images/'
 if os.path.exists(img_dir) is False:
     os.mkdir(img_dir)
 img_dir_files = os.listdir(img_dir)
 
 
 for wiki_img in list(wiki_images):
-    info = wiki_img.imageinfo
-    #pprint(info)
-    size = info['size']    
-    url = info['url'].replace(" ","_")
-    #print(url)
-    if wiki_img.page_title not in img_dir_files:
-        try:
-            urllib.request.urlretrieve(info['url'], img_dir + wiki_img.page_title)
-            print('Image: {} not found. Downloading it!'.format(wiki_img.page_title))
-        except:
-            print("Error downloading {}".format(url))
-    else:
-        print('Image: {} in folder!'.format(wiki_img.page_title))
-        local_img_size = os.path.getsize(img_dir + wiki_img.page_title)
-        if local_img_size != size:
+    # Download image if is png, jpg, gif and is being used
+    if wiki_img.page_title not in img_dir_files and \
+        ((wiki_img.page_title).split('.')[-1]).lower() \
+        in ['png', 'jpg', 'gif'] and \
+            len(list(wiki_img.imageusage())) > 0:
+        info = wiki_img.imageinfo
+        # pprint(info)
+        height = info['height']
+        size = info['size']
+        if height > set_hight:  # request a thumb
+            api_thumb_action = 'action=query&format=json&prop=imageinfo&iiprop=url&iiurlheight={height}&titles={page}'.format(
+                height= str(set_hight) ,
+                page=wiki_img.name.replace(" ", "_"))
+            api_query = api + api_thumb_action
+            with urllib.request.urlopen(api_query) as response:
+                api_thumb = response.read().decode('utf-8')
+                api_thumb = json.loads(api_thumb)
+                # import pdb; pdb.set_trace()
+                api_thumb_key = [*api_thumb['query']['pages'].keys()][0]
+                url = api_thumb['query']['pages'][api_thumb_key]['imageinfo'][0]['thumburl']
+                # print(url)
+        else:  # request regular image
+            url = info['url'].replace(" ", "_")
+
+
+        if wiki_img.page_title not in img_dir_files:
+            img_title = wiki_img.page_title.replace(' ', '_')
             try:
-                urllib.request.urlretrieve(info['url'], img_dir + wiki_img.page_title)
-                print('Image: {} in folder has DIFFERENT SIZE! Local {}, wiki {}. Downloading it'.format(wiki_img.page_title,local_img_size, size ))
+                urllib.request.urlretrieve(url, img_dir + img_title)
+                print('Image: {} not found. Downloading it!'.format(
+                    wiki_img.page_title))
             except:
                 print("Error downloading {}".format(url))
-
+        else:
+            print("{} already present in images dir".format(
+                wiki_img.page_title))
