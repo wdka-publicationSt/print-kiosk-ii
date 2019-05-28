b354e3250b33cce111d58d850128c03e7069539f
manetta <mail@manettaberends.nl>
2019-05-28 18:31:11 +0200

    removed the parse_html function, we don't use it

--- functions.py
+++ functions.py
@@ -6,35 +6,26 @@ from nltk.tokenize import RegexpTokenizer
 tokenizer = RegexpTokenizer(r'\w+') # initialize tokenizer
 from yaml import load, Loader, Dumper
 
-def remove_wiki_elements(html):
-	soup = BeautifulSoup(html, "html5lib")
-
-	# remove the contribute banner + recent articles suggestions banner
+def remove_wiki_elements(soup):
+	"""
+		Removing the contribute banner + recent articles suggestions banner from all the pages.
+	"""	
+	# Remove contribute banner
 	contribute = soup.find(id='contribute-wrapper')
 	if contribute != None:
 		contribute.clear()
+	# Remove suggestions banner
 	suggestions = soup.find_all('div', class_='oneline-wrapper')
 	if len(suggestions) > 0:
 		suggestions[0].clear()
 
 	html = str(soup.body)
 	return html
-
-def parse_html(html):
-	soup = BeautifulSoup(html, "html5lib")
-
-	# remove the contribute banner + recent articles suggestions banner
-	contribute = soup.find(id='contribute-wrapper')
-	if contribute != None:
-		contribute.clear()
-	suggestions = soup.find_all('div', class_='oneline-wrapper')
-	if len(suggestions) > 0:
-		suggestions[0].clear()
-
-	plain = soup.text
-	return plain
-
+	
 def tokenize(plain):
+	"""
+		Splitting plain text up into a list of words.
+	"""
 	words = tokenizer.tokenize(plain)
 	return words
 
@@ -44,7 +35,6 @@ def count_article_length(html):
 	length = len(words)
 	print('length of this article: ', length)
 	return length
-
 
 def get_yaml_data(filename):
     yaml_f = open(filename, "r").read()
